d6e0fda65377b2d3ee65c1cdc60e24eb5ab2fcd4
StuGen <StuGen@bcs.vusd.solanocoe.k12.ca.us>
2016-03-09 15:32:21 -0800

    Add files that were not added

---
+++ src/org/usfirst/frc/team5496/robot/commands/DriveRobotUseRamp.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team5496.robot.commands;
+
+import org.usfirst.frc.team5496.robot.Robot;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Used when we need to run both DriveRobot and UseRamp at the same time.
+ */
+public class DriveRobotUseRamp extends CommandGroup {
+
+	public DriveRobotUseRamp() {
+		// Add Commands here:
+		// e.g. addSequential(new Command1());
+		// addSequential(new Command2());
+		// these will run in order.
+
+		// To run multiple commands at the same time,
+		// use addParallel()
+		// e.g. addParallel(new Command1());
+		// addSequential(new Command2());
+		// Command1 and Command2 will run in parallel.
+
+		// A command group will require all of the subsystems that each member
+		// would require.
+		// e.g. if Command1 requires chassis, and Command2 requires arm,
+		// a CommandGroup containing them would require both the chassis and the
+		// arm.
+
+		addParallel(Robot.driveRobot);
+		addParallel(Robot.useRamp);
+
+		try {
+			System.out.println("sampImage starting");
+			addSequential(Robot.sampImage);
+		} catch (Throwable t) {
+			System.out.println(t.toString());
+			System.out.println("SampleImage is the problem");
+		}
+
+	}
+
+}
